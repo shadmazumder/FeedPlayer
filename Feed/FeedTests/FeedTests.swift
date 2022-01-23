d919b74e38d8a0c13a40ea425c3c9ad34ba07623
Shad Mazumder <shadmazumder@gmail.com>
2022-01-24 00:11:27 +0600

    Local loader delivers error on client error

--- Feed/FeedTests/FeedTests.swift
+++ Feed/FeedTests/FeedTests.swift
@@ -21,6 +21,7 @@ struct LocalLoader<T: Decodable>: Loader{
     public typealias LoaderError = Error
     
     public enum Error: Swift.Error {
+        case resourceNotFound
     }
     
     private let uri: String
@@ -33,7 +34,7 @@ struct LocalLoader<T: Decodable>: Loader{
     
     func load(completion: @escaping ((Result<APIModel, LoaderError>) -> Void)){
         client.get(from: uri){ _ in
-            
+            completion(.failure(.resourceNotFound))
         }
     }
 }
@@ -80,7 +81,12 @@ class FeedTests: XCTestCase {
     }
     
     func test_load_deliverError_onClientError() {
-        
+        let (sut, client) = makeSUT()
+        let anyError = NSError(domain: "Any doamin", code: 0)
+
+        expect(sut, tocompleteWith: .failure(LocalLoader.Error.resourceNotFound)) {
+            client.completeWithError(anyError)
+        }
     }
     
     // MARK: - Helper
@@ -93,4 +99,25 @@ class FeedTests: XCTestCase {
     }
     
     private let anyURI = "any-uri"
+    
+    private func expect(_ sut: LocalLoaderStringType, tocompleteWith expectedResult: LocalLoaderStringType.Result, when action: ()-> Void, file: StaticString = #file, line: UInt = #line) {
+        let exp = expectation(description: "Waiting for the client")
+
+        sut.load { result in
+            switch (result, expectedResult) {
+            case let (.success(recieved), .success(expected)):
+                XCTAssertEqual(recieved, expected, file: file, line: line)
+            case let (.failure(receivedError), .failure(expectedError)):
+                XCTAssertEqual(receivedError.localizedDescription, expectedError.localizedDescription, file: file, line: line)
+
+            default:
+                XCTFail("Expected \(expectedResult) but got \(result)", file: file, line: line)
+            }
+            exp.fulfill()
+        }
+
+        action()
+
+        wait(for: [exp], timeout: 1.0)
+    }
 }
