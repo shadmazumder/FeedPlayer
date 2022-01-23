68474a96caf75ece9a399a3e1ba3c54cf0922351
Shad Mazumder <shadmazumder@gmail.com>
2022-01-24 01:11:32 +0600

    Moving LocalLoader to its own file

--- Feed/Feed/Loader/LocalLoader.swift
+++ Feed/Feed/Loader/LocalLoader.swift
@@ -6,3 +6,51 @@
 //
 
 import Foundation
+
+public class LocalLoader<T: Decodable>: Loader{
+    public typealias APIModel = T
+    public typealias LoaderError = Error
+    
+    public enum Error: Swift.Error {
+        case resourceNotFound
+        case invalidData
+        case decoding(DecodingError)
+    }
+    
+    private let uri: String
+    private let client: Client
+    
+    public init(uri: String, client: Client){
+        self.uri = uri
+        self.client = client
+    }
+    
+    public func load(completion: @escaping ((Result<APIModel, LoaderError>) -> Void)){
+        client.get(from: uri){ [weak self] result in
+            switch result {
+            case let .success(data):
+                self?.mapSuccessFrom(data, completion)
+            default:
+                completion(.failure(.resourceNotFound))
+            }
+        }
+    }
+    
+    private func mapSuccessFrom(_ data: Data, _ completion: @escaping ((Result<APIModel, LoaderError>) -> Void)) {
+        do {
+            let jsonDecoder = JSONDecoder()
+            let root = try jsonDecoder.decode(T.self, from: data)
+            completion(.success(root))
+        } catch {
+            mapErrorFrom(error, completion)
+        }
+    }
+    
+    private func mapErrorFrom(_ error: Swift.Error, _ completion: ((Result<APIModel, LoaderError>) -> Void)) {
+        if let decodingError = error as? DecodingError {
+            completion(.failure(.decoding(decodingError)))
+        }else {
+            completion(.failure(.invalidData))
+        }
+    }
+}
