00aa4e3aa4539fefe7b133ed829491d5f2f1e2b9
Shad Mazumder <shadmazumder@gmail.com>
2022-01-24 02:14:45 +0600

    Renaming method

--- Feed/FeediOSTests/FeediOSTests.swift
+++ Feed/FeediOSTests/FeediOSTests.swift
@@ -11,11 +11,11 @@ import FeediOS
 
 class FeediOSTests: XCTestCase {
     func test_loadFromStoryboard_returnsFeedViewController() {
-        XCTAssertTrue(launchesViewControllerFromPostSotyboard() is FeedsViewController, "Initial ViewController is not FeedsViewController")
+        XCTAssertTrue(launchesViewControllerFromFeedsSotyboard() is FeedsViewController, "Initial ViewController is not FeedsViewController")
     }
     
     // MARK: - Helper
-    private func launchesViewControllerFromPostSotyboard() -> UIViewController? {
+    private func launchesViewControllerFromFeedsSotyboard() -> UIViewController? {
         let bundle = Bundle(for: FeedsViewController.self)
         let storyboard = UIStoryboard(name: "Feeds", bundle: bundle)
         return storyboard.instantiateInitialViewController()
