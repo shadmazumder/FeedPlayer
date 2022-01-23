4cdbc823a46c5d4bd13b751c8f3bc87e8946d3ff
Shad Mazumder <shadmazumder@gmail.com>
2022-01-24 01:30:24 +0600

    Initiating LocalClient does not called completion

--- Feed/FeedTests/LocalClientTests.swift
+++ Feed/FeedTests/LocalClientTests.swift
@@ -6,6 +6,26 @@
 //
 
 import XCTest
+import Feed
+
+struct LocalClient: Client {
+    func get(from uri: String, completion: @escaping (Client.Result) -> Void) {
+        
+    }
+}
 
 class LocalClientTests: XCTestCase {
+    func test_init_doesNotPerformCompletion() {
+        var completionCalled = false
+        let sut = LocalClient()
+        
+        sut.get(from: anyUri) { _ in
+            completionCalled = true
+        }
+        
+        XCTAssertFalse(completionCalled)
+    }
+    
+    // MARK: - Helper
+    private let anyUri = "any-uri"
 }
