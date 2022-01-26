dd5802bd48ae4498893eb663702661548352c8cb
Shad Mazumder <shadmazumder@gmail.com>
2022-01-26 16:28:05 +0600

    Moving FeedsViewControllerComposer into its own file

--- Feed/FeedPlayerTests/ViewControllerComposerTests.swift
+++ Feed/FeedPlayerTests/ViewControllerComposerTests.swift
@@ -10,52 +10,6 @@ import FeedPlayer
 import Feed
 import FeediOS
 
-class FeedsViewControllerComposer{
-    private let jsonFeedFileName: String
-    private let logger: Logger
-    
-    init(jsonFeedFileName: String, logger: Logger = FeedLogger()) {
-        self.jsonFeedFileName = jsonFeedFileName
-        self.logger = logger
-    }
-    
-    var feedsViewController: FeedsViewController{
-        let feedsViewController = feedStoryboard.instantiateInitialViewController() as! FeedsViewController
-        defer{ configure(feedsViewController) }
-        return feedsViewController
-    }
-    
-    private let feedStoryboard: UIStoryboard = {
-        let bundle = Bundle(for: FeedsViewController.self)
-        return UIStoryboard(name: "Feeds", bundle: bundle)
-    }()
-    
-    private func configure(_ feedsViewController: FeedsViewController){
-        feedsViewController.playerDelegate = FeedPlayer(feedLogger: logger)
-        feedsViewController.errorHandler = FeedErrorHandler(presentingViewController: feedsViewController)
-        configureLocalLoader(for: feedsViewController, logErrorOn: logger)
-    }
-    
-    private func configureLocalLoader(for feedsViewController: FeedsViewController, logErrorOn logger: Logger){
-        let failableLoader = makeFailableLocalLoader()
-        if let loader = failableLoader.localLoader {
-            feedsViewController.loader = loader
-        }else{
-            logger.logMessage(failableLoader.errorMessage)
-        }
-    }
-    
-    private func makeFailableLocalLoader() -> (localLoader: LocalLoader?, errorMessage: String?){
-        let allJsonPaths = Bundle.main.paths(forResourcesOfType: ".json", inDirectory: nil)
-        
-        guard let path = allJsonPaths.filter({ $0.contains(jsonFeedFileName) }).first else {
-            return (nil, "Json file was not found on the path")
-        }
-        let localLoader = LocalLoader(uri: path, client: LocalClient(), feedGenerator: FeedGenerator())
-        return (localLoader, nil)
-    }
-}
-
 class ViewControllerComposerTests: XCTestCase {
     func test_invalidFeedJsonPath_doesNotInitiateLoader() {
         let (_, feedViewController) = makeSUT(anyInvalidJsonFileName)
